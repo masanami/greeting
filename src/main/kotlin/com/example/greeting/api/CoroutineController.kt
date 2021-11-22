c61f5e38971ab76a8dda1506fe2b0328818fd51e
masanami <masa.namihira@gmail.com>
2021-11-22 11:16:25 +0900

    fix

--- src/main/kotlin/com/example/greeting/api/CoroutineController.kt
+++ src/main/kotlin/com/example/greeting/api/CoroutineController.kt
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("greeting")
-class GreetingController {
+@RequestMapping("coroutine")
+class CoroutineController {
 
     @GetMapping
     fun get(): String{
