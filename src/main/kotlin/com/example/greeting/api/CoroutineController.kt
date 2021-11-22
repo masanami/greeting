bc83f16ba70013ed678da0b8972e68b8e4b76cdc
masanami <masa.namihira@gmail.com>
2021-11-22 11:15:45 +0900

    add coroutine

---
+++ src/main/kotlin/com/example/greeting/api/CoroutineController.kt
@@ -0,0 +1,23 @@
+package com.example.greeting.api
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("greeting")
+class GreetingController {
+
+    @GetMapping
+    fun get(): String{
+        CoroutineScope(Dispatchers.Default).launch {
+            delay(3000)
+            println("コルーチンの中")
+        }
+        return "Hello World!!"
+    }
+}
